1c84f7f8e8b8f1d04898024925c0414afb9d22f6
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-05-25 23:04:36 +0200

    update java

--- integration/java/Main.java
+++ integration/java/Main.java
@@ -1,15 +1,20 @@
 package org.typeschema.generator;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        String input = Files.readString("../input.json");
+    public static void main(String[] args) throws IOException {
+        String input = Files.readString(Path.of("../input.json"));
 
         ObjectMapper objectMapper = new ObjectMapper();
 
         News data = objectMapper.readValue(input, News.class);
 
-        String output = objectMapper.writeValueAsString(data)
+        String output = objectMapper.writeValueAsString(data);
 
-        Files.write("../output.json", output);
+        Files.write(Path.of("../output.json"), output.getBytes());
     }
 }
