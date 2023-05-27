a17aad534b5ae3601124d9e3d47184c7a8389e1d
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-05-27 18:12:10 +0200

    add doc

--- integration/typescript/main.js
+++ integration/typescript/main.js
@@ -5,6 +5,8 @@ const process = require('process');
 try {
     const input = fs.readFileSync('../input.json', 'utf8');
 
+    // TypeScript is a special case, we cant actually use the generated types since the types only describe the JSON
+    // payload so we only deserialize and serialize the JSON payload
     const news = JSON.parse(input);
 
     const output = JSON.stringify(news);
