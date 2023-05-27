4c6ed3566b8232e61ae337964c140ebd33d7ed18
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-05-27 10:48:40 +0200

    update rust

--- integration/rust/main.rs
+++ integration/rust/main.rs
@@ -1,13 +1,13 @@
 use std::env;
 use std::fs;
-use serde_json::Result;
+use serde::{Serialize, Deserialize};
 
 fn main() {
     let input = fs::read_to_string("../input.json").expect("Could not read input");
 
-    let news: News = serde_json::from_str(input).unwrap();
+    let news: News = serde_json::from_str(&input).unwrap();
 
-    let output = serde_json::to_str(news);
+    let output = serde_json::to_string(&news).unwrap();
 
     fs::write("../output.json", output).expect("Could not write output");
 }
