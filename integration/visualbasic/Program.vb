b7cb87af403d36733c7e547695609dbdfcf829f4
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-05-27 19:08:40 +0200

    add vb

---
+++ integration/visualbasic/Program.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Text.Json
+Imports Generator.Generator
+Imports Microsoft.VisualBasic.FileIO
+
+Module Program
+    Sub Main(args as String())
+        Dim input As String
+        Dim output As String
+        Dim news As News
+
+        input = FileSystem.ReadAllText("../input.json")
+
+        news = JsonSerializer.Deserialize(Of News)(input)
+
+        output = JsonSerializer.Serialize(news)
+
+        FileSystem.WriteAllText("../output.json", output, True)
+    End Sub
+End Module
