16a29d749248e9ccfdb75219e185a037ec3d4505
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-11-10 12:21:24 +0100

    use object mapper

--- integration/php/main.php
+++ integration/php/main.php
@@ -2,22 +2,19 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-require_once __DIR__ . '/Location.php';
-require_once __DIR__ . '/Web.php';
-require_once __DIR__ . '/Meta.php';
 require_once __DIR__ . '/Author.php';
+require_once __DIR__ . '/Location.php';
+require_once __DIR__ . '/Meta.php';
 require_once __DIR__ . '/News.php';
 
+$objectMapper = (new \PSX\Schema\ObjectMapper(new \PSX\Schema\SchemaManager()));
 $data = json_decode(file_get_contents(__DIR__ . '/../input.json'));
 
-$schemaManager = new \PSX\Schema\SchemaManager();
-$schema = $schemaManager->getSchema(\Generator\News::class);
-
 try {
-    $news = (new \PSX\Schema\SchemaTraverser())->traverse($data, $schema, new \PSX\Schema\Visitor\TypeVisitor());
+    $news = $objectMapper->read($data, \PSX\Schema\SchemaSource::fromClass(\Generator\News::class));
 
     file_put_contents(__DIR__ . '/../output.json', \json_encode($news));
-} catch (\PSX\Schema\Exception\ValidationException $e) {
+} catch (\Throwable $e) {
     // the validation failed
     echo $e->getMessage();
     exit(1);
