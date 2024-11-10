1aedc0ac4a8ea20e820e1283623dbe2a0caeeccd
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-11-10 12:28:56 +0100

    use pydantic

--- integration/python/main.py
+++ integration/python/main.py
@@ -3,11 +3,7 @@ from pathlib import Path
 
 input = Path('../input.json').read_text()
 
-# @TODO currently we dont use the @dataclass classes since we have not found an
-# easy way to serialize or deserialize those natively please let us know if you
-# have some ideas
-
-news = json.loads(input)
+news = News.model_validate_json(input)
 
 output = json.dumps(news)
 
